b486756b722a959ba0b4ffcf88a1883ea24d4d77
Rookie <rookie@iMac-2.local>
2026-06-04 13:44:18 +0200

    Holaaa

--- src/app/supabase.client.ts
+++ src/app/supabase.client.ts
@@ -3,4 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://supabase.com/dashboard/project/ukbhxvdbqotfykwbvxej';
 const supabaseKey = 'sb_publishable_owzSElL6pHEJ7QFrJH1_WA_ApRMFy0T';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://supabase.com/dashboard/project/ukbhxvdbqotfykwbvxej',
+    supabaseAnonKey: 'sb_publishable_owzSElL6pHEJ7QFrJH1_WA_ApRMFy0T'
+  };
+
+
+export const supabase = createClient(
+    'https://ukbhxvdbqotfykwbvxej.supabase.co',
+    'sb_publishable_owzSElL6pHEJ7QFrJH1_WA_ApRMFy0T'
+  );
